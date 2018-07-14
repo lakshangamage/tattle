808fb33299f2c59ad9d6709a051df45ad2d71515
Lakshan Gamage <lakshangamage.13@cse.mrt.ac.lk>
2018-07-14 15:49:58 +0530

    initial project

---
+++ app/src/main/java/com/ceyentra/tattle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ceyentra.tattle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
